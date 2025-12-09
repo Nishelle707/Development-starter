9dc90094c130e165ce6d860f67760910b525935b
Nishelle707 <nishellejoel@gmail.com>
2025-12-09 10:26:38 +0530

    Add files via upload

--- H.w 2.py
+++ H.w 2.py
@@ -1,9 +1,10 @@
-Name = input("Enter a name:")
+name = input("Enter a name: ")
 
-if name = Joshua:
+if name == "Joshua":
     print("15/2/2010, Joshua's Birthday")
-elif name = Anna:
-    print("1/7/2011, Anna's Birthday") 
-else: 
-print ("Birthday not found")      
 
+elif name == "Anna":
+    print("1/7/2011, Anna's Birthday")
+
+else:
+    print("Birthday not found")
